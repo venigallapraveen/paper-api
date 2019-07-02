fa93303c9feb36bc63d58d1bb04c6c4edf791ae3
Praveen Venigalla <praveenv.aiesec@gmail.com>
2019-07-01 20:21:15 -0400

    added getScreams method to the firebase

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,27 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello from Firebase!");
+});
+
+exports.getScreams = functions.https.onRequest((req, res) => {
+  admin
+    .firestore()
+    .collection("screams")
+    .get()
+    .then(data => {
+      let screams = [];
+      data.forEach(doc => {
+        screams.push(doc.data());
+      });
+
+      return res.json(screams);
+    })
+    .catch(err => console.error(err));
+});
