8dffae85bc7880b5ef5bd7680681cea8d403c681
Praveen Venigalla <praveenv.aiesec@gmail.com>
2019-07-02 21:20:17 -0400

    notification on user image change

--- functions/index.js
+++ functions/index.js
@@ -106,3 +106,25 @@ exports.createNotificationOnComment = functions.firestore
         return;
       });
   });
+
+exports.onUserImageChange = functions.firestore
+  .document("/users/{userId}")
+  .onUpdate(change => {
+    console.log(change.before.data());
+    console.log(change.after.data());
+    if (change.before.data().imageUrl !== change.after.data().imageUrl) {
+      console.log("image has changed");
+      const batch = db.batch();
+      return db
+        .collection("screams")
+        .where("userHandle", "==", change.before.data().handle)
+        .get()
+        .then(data => {
+          data.forEach(doc => {
+            const scream = db.doc(`/screams/${doc.id}`);
+            batch.update(scream, { userImage: change.after.data().imageUrl });
+          });
+          return batch.commit();
+        });
+    } else return true;
+  });
