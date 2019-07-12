33820a82b81c09aeacd03a753f3cb5d67eb85dc5
Praveen Venigalla <praveenv.aiesec@gmail.com>
2019-07-11 21:31:23 -0400

    added cors

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,9 @@
 const functions = require("firebase-functions");
 const app = require("express")();
 const FBAuth = require("./util/fbAuth");
+const cors = require("cors");
+
+app.use(cors());
 
 const { db } = require("./util/admin");
 
